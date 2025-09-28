0930b274e1de182d5e50b107bee35db8ba383440
viveak910 <1602-23-737-191@vce.ac.in>
2025-09-28 12:36:34 +0530

    payment page changes

--- .history/src/forms/PaymentStep_20250928123558.js
+++ .history/src/forms/PaymentStep_20250928123558.js
@@ -32,18 +32,18 @@ export default function PaymentStep({
       <div style={{ fontFamily: "Arial, sans-serif", lineHeight: "1.6", backgroundColor: "#000", color: "#fff", padding: "20px" }}>
         <h1>Dear Delegate,</h1>
 
-    <p>
-      Thank you for your interest in our{" "}
-      <strong>Dynamic Round registrations</strong>. We will receive your
-      registration after submission, and our team will reach out to you within
-      approximately two to three business days. We kindly request your patience
-      during this period.
-    </p>
+<p>
+  Thank you for your interest in our{" "}
+  <strong>Dynamic Round registrations</strong>. We will receive your
+  registration after submission, and our team will reach out to you within
+  approximately two to three business days. We kindly request your patience
+  during this period.
+</p>
 
-    <p>
-      Thank you. We appreciate your understanding and look forward to
-      connecting with you soon.
-    </p>
+<p>
+  Thank you. We appreciate your understanding and look forward to
+  connecting with you soon.
+</p>
 
         <p>
           Best regards, <br />
